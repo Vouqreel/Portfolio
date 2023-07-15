ac80fe6e8b057c9aca4564bdd5691de6fe84d1b8
Vazgen <vouqreel@gmail.com>
2023-07-16 01:33:54 +0500

    optimization and fixing bugs

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
@@ -9,7 +8,7 @@ const Tech = () => {
 		<div className="flex flex-row flex-wrap justify-center gap-10">
 			{technologies.map((technology) => (
 				<div className="w-28 h-28" key={technology.name}>
-					<BallCanvas icon={technology.icon} />
+					<BallCanvas icons={[technology.icon]} />
 				</div>
 			))}
 		</div>
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -9,23 +9,22 @@ const Ball = (props) => {
 
 	return (
 		<Float speed={3} rotationIntensity={2} floatIntensity={3}>
-			<ambientLight intensity={0.25} />
-			<directionalLight position={[0, 0, 0.05]} />
 			<mesh castShadow receiveShadow scale={2.75}>
 				<icosahedronGeometry args={[1, 1]} />
-				<meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+				<meshBasicMaterial polygonOffset polygonOffsetFactor={-5} flatShading />
 				<Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} flatShading />
 			</mesh>
 		</Float>
 	);
 };
-
-const BallCanvas = ({ icon }) => {
+const BallCanvas = ({ icons }) => {
 	return (
 		<Canvas dpr={[1, 2]} gl={{ preserveDrawingBuffer: true }}>
 			<Suspense fallback={<CanvasLoader />}>
 				<OrbitControls enableZoom={false} />
-				<Ball imgUrl={icon} />
+				{icons.map((icon, index) => (
+					<Ball key={index} imgUrl={icon} />
+				))}
 			</Suspense>
 
 			<Preload all />
